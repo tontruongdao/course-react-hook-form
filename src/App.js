1d872fdd584bfd87ff0a2aafc81e8a9607d7a0c1
truongsinistar <truong@sinistar.ca>
2021-06-21 09:50:53 -0400

    project initiation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import { BroserRrouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path="/" component={step1} />
+      <Route exact path="/step2" component={step2} />
+      <Route exact path="/step3" component={step3} />
+      <Route exact path="/result" component={result} />
+    </Router>
   );
 }
 
